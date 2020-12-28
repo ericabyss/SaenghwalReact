b2f34f3c5f03bb6782e57580ca7a388c2924f035
ericabyss <erica051746@naver.com>
2020-12-28 18:20:56 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class Subject extends Component { 
+  render() { //클래스 안에서는 FUNCTION 생략
+    return (
+      <header> 
+        {/* 하나의 최상위 태그만 써야함 */}
+        <h1>WEB</h1>
+        world wide web!
       </header>
-    </div>
-  );
+    );
+  }
+}
+
+
+//컴포넌트를 만드는 곳
+class App extends Component { //App이라는 클래스, Component라는 리엑트가 가지고있는 속성, 유사 자바스크립트  
+  render() { //render라는 메소드를 가지고 있음.
+    return (
+      <div className="App">
+        <Subject />
+      </div>
+    );
+  }
 }
 
 export default App;
